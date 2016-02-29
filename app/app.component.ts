a57b034d6871a70bd7b1d8a8bc72509bf60e20c9
TJ VanToll <tj.vantoll@gmail.com>
2016-02-29 12:50:38 -0500

    Upgrading to Angular beta 7, and TypeScript 1.8.2

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,6 @@ import {Component} from "angular2/core";
 
 @Component({
   selector: "my-app",
-  template: `<Label text="hello world 2"></Label>`
+  template: "<label text='hello world'></label>"
 })
 export class AppComponent {}
